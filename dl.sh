18af8024a3623f3e6606778c90acbeeed46e53b5
Roman Martsev <53344706+Windemiatrix@users.noreply.github.com>
2025-07-25 18:34:10 +0500

    fix: edit arch

--- dl.sh
+++ dl.sh
@@ -3,13 +3,13 @@ set -eu
 
 BIN_DIR="/usr/local/bin/"
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-ARCH=$(uname -m | sed 's/x86_64/amd64/' | sed 's/aarch64/arm64/')
+ARCH=$(uname -m | sed 's/amd64/x86_64/' | sed 's/aarch64/arm64/')
 
 echo "OS: ${OS}"
 echo "ARCH: ${ARCH}"
 
 [ -z "$(command -v curl)" ] && echo 'curl must be installed' && exit 1
-[ "$OS/$ARCH" != "linux/amd64" ] && [ "$OS/$ARCH" != "linux/arm64" ] && [ "$OS/$ARCH" != "darwin/amd64" ] && [ "$OS/$ARCH" != "darwin/arm64" ] && echo "Unsupported platform $OS/$ARCH. Supports linux/amd64 and linux/arm64." && exit 1
+[ "$OS/$ARCH" != "linux/x86_64" ] && [ "$OS/$ARCH" != "linux/arm64" ] && [ "$OS/$ARCH" != "darwin/x86_64" ] && [ "$OS/$ARCH" != "darwin/arm64" ] && echo "Unsupported platform $OS/$ARCH. Supports linux/amd64 and linux/arm64." && exit 1
 
 dl_url=$(curl -s https://api.github.com/repos/google/go-jsonnet/releases/latest | grep "browser_download_url" | grep -i "${OS}_${ARCH}.tar.gz" | cut -d : -f 2- | tr -d \")
 echo "Download URL: ${dl_url}"
